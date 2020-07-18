7f9628f166c5c3cc5fd585ea152cb054fead4bec
RamaKoteswararao Gutta <gutta.ramakoteswararao@gmail.com>
2020-07-18 22:50:58 +0530

    input field
    input field

--- testingangular/src/app/app.module.ts
+++ testingangular/src/app/app.module.ts
@@ -4,6 +4,8 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NewCmpComponent } from './new-cmp/new-cmp.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatInputModule} from '@angular/material/input';
 
 @NgModule({
   declarations: [
@@ -12,7 +14,9 @@ import { NewCmpComponent } from './new-cmp/new-cmp.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatInputModule
   ],
   providers: [],
   bootstrap: [AppComponent]
